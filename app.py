bb5cf9d6b90000430c218a31d714e70f5c55f3ba
VagnerMesquita <69486774+VagnerMesquita@users.noreply.github.com>
2024-03-20 14:01:26 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'All is well...'
+@app.route("/pudim")
+def api():
+    headers = {"X-Parse-Application-Id":"lx03BLJ08F9S9la2EcWBKfdlZ1aAFrSPwBVweGlW",
+            "X-Parse-REST-API-Key": "CygcDai6A2NYfWvS4C4JWyV0G4ur6grCqlLtkNki"}
+
+
+    r = requests.post(f'https://parseapi.back4app.com/functions/get-norma', headers=headers, data= {"normaId": "qyfDq57psG"} )
+    channelId = r.json()['result'][0]['channelId']
+    channelName = r.json()['result'][0]['channelName']
+
+    print(channelName)
+    return {"message": channelName}
